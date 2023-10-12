0eea9e17922eed4620555c6ed97a2501c9a47870
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-10-12 17:23:13 +0300

    update contact.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,20 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const ctrl = require("../../controllers/contacts");
+const validateBody = require("../../middlewares/validateBody");
+const schemas = require("../../schemas/contacts");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', ctrl.ListContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId' , ctrl.getContactById);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/' , validateBody(schemas.addSchema), ctrl.addContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId' , ctrl.removeContact);
 
-module.exports = router
+router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateContact);
+
+
+
+module.exports = router;
