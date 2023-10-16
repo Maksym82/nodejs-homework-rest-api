6a46529ee48b75bdf04dd8b7a650ca45e900bc06
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-10-16 15:55:05 +0300

    fix

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+// require('dotenv').config()
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = process.env.DB_HOST;
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
