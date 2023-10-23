6d7044189d53f316502e31dfe88a6c9684f911e2
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-10-23 17:54:13 +0300

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,7 +14,7 @@ router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
 
 router.delete("/:contactId", isValidId, ctrl.removeContact);
 
-router.put("/:contactId", isValidId, validateBody(schemas.addSchema), ctrl.updateContact);
+router.put("/:contactId", isValidId, ctrl.updateContact);
 
 router.patch("/:contactId/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateStatusContact);
 
