4f47226fb53bcfee058cb948d3e446aa9e382a76
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-11-11 18:44:58 +0200

    create verifyEmail.js

---
+++ helpers/verifyEmail.js
@@ -0,0 +1,15 @@
+const verifyEmail = (email, verificationToken) => {
+  return {
+    to: email,
+    subject: `Verify email`,
+    html: `<p>To confirm your registration, please click on link below</p>
+          <p>
+            <a target="_blank" href="${process.env.BASE_URL}/api/users/verify/${verificationToken}">Click verify email</a>
+          </p>`,
+    text: `To confirm your registration, please click on link below\n
+      ${process.env.BASE_URL}/api/users/verify/${verificationToken}
+       `,
+  };
+};
+
+module.exports = verifyEmail;
